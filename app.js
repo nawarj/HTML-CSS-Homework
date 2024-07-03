0c118b5cda2b49748c4a37fe8145c9395daac16c
nawarj <nawarjdeed035@gmail.com>
2024-07-03 08:42:51 +0300

    make some new changes

--- app.js
+++ app.js
@@ -13,37 +13,30 @@ function chkPassword() {
         return false;
     } else
         alert("Done!")
-    // return true;
-
 }
 
 document.getElementById("second").onblur = chkPassword;
 document.getElementById("myForm").onsubmit = chkPassword;
 
-// let a = 20;
-// let b = "10";
-// let c = true;
-// console.log();
-
 const heading = document.getElementById("h1")
 const home = document.getElementById("home")
 const button = document.createElement("button")
+const resetBtn = document.createElement("button")
 
-// style the element
+// --------------- style the element
 button.innerText = "Add +1"
 button.style.padding = "12px 18px"
 button.style.marginTop = "20px"
 button.style.cursor = 'pointer'
 button.style.backgroundColor = '#333'
 
-// onclick change style
-// button.addEventListener('mouseenter', () => {
-//     button.style.backgroundColor = '#fff'
-// })
+// --------------- style the element
+resetBtn.innerText = "Reset"
+resetBtn.style.padding = "12px 18px"
+resetBtn.style.marginTop = "20px"
+resetBtn.style.cursor = 'pointer'
+resetBtn.style.backgroundColor = 'red'
 
-// button.addEventListener('mouseleave', () => {
-//     button.style.backgroundColor = '#333'
-// })
 
 let toggle = false;
 
@@ -59,27 +52,67 @@ button.addEventListener('click', () => {
     }
 })
 
-// add attribute to BTN
+// --------------- add attribute to BTN
 // button.setAttribute('disabled', '')
 button.setAttribute('id', 'btn')
 
-// Create a counter
+// --------------- Create a counter
 const counter = document.createElement("p")
 
 let count = 0;
 
-// style the element
-counter.innerText = count
+// --------------- style the element
+counter.innerText = `Count: ${count}`
 counter.style.marginTop = '20px'
 
-// add the element to the section
-home.appendChild(button)
-home.appendChild(counter)
+
+
+
+// --------------- add the elements to the section
+
+// home.appendChild(button)
+// home.appendChild(resetBtn)
+// home.appendChild(counter)
+
+// --------------- function 
+function addChild(parent, child) {
+    parent.appendChild(child)
+}
+
+addChild(home, button)
+addChild(home, resetBtn)
+addChild(home, counter)
+
+
+
 
 button.addEventListener("click", function () {
     count++
-    counter.innerText = count
+    counter.innerText = `Count: ${count}`
+
+    // --------------- add item "P" every click
+
+    // create a new element
+    const newP = document.createElement("p")
+
+    // style the new element
+    newP.style.color = "#fff"
+    newP.style.fontWeight = "600"
+    newP.innerHTML = `item ${count}`
+
+    // add the new element to the Document
+    addChild(home, newP)
+
+    // Reset button
+    resetBtn.addEventListener('click', function () {
+        newP.remove()
+        // reset the count
+        count = 0
+        // update the reset on the Document
+        counter.innerText = `Count: ${count}`
+    })
 })
+
 
 
 const backToTop = document.querySelector('.toTop')
@@ -90,5 +123,4 @@ backToTop.addEventListener("click", () => {
         behavior: "smooth",
     })
 })
-
 
