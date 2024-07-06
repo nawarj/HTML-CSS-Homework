8f268c0f0f8812c4e52a4d4de58278f7f4c53ace
nawarj <nawarjdeed035@gmail.com>
2024-07-06 21:45:15 +0300

    Little Changes

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ button.style.padding = "12px 18px"
 button.style.marginTop = "20px"
 button.style.cursor = 'pointer'
 button.style.backgroundColor = '#333'
+button.style.border = "none"
 
 // --------------- style the element
 resetBtn.innerText = "Reset"
@@ -36,6 +37,7 @@ resetBtn.style.padding = "12px 18px"
 resetBtn.style.marginTop = "20px"
 resetBtn.style.cursor = 'pointer'
 resetBtn.style.backgroundColor = 'red'
+resetBtn.style.border = "none"
 
 
 let toggle = false;
@@ -53,7 +55,7 @@ button.addEventListener('click', () => {
 })
 
 // --------------- add attribute to BTN
-// button.setAttribute('disabled', '')
+// button.setAttribute('disabled', '') 
 button.setAttribute('id', 'btn')
 
 // --------------- Create a counter
@@ -66,55 +68,52 @@ counter.innerText = `Count: ${count}`
 counter.style.marginTop = '20px'
 
 
-
-
 // --------------- add the elements to the section
 
-// home.appendChild(button)
-// home.appendChild(resetBtn)
-// home.appendChild(counter)
+// home.appendChild(button)     [*** This is the original way to append a child ***]
+// home.appendChild(resetBtn)   [*** This is the original way to append a child ***]
+// home.appendChild(counter)    [*** This is the original way to append a child ***]
 
-// --------------- function 
+// --------------- function  [*** This is an example about functions ***]
 function addChild(parent, child) {
     parent.appendChild(child)
 }
 
-addChild(home, button)
-addChild(home, resetBtn)
-addChild(home, counter)
+addChild(home, button)      //[*** This is an example about functions ***]
+addChild(home, resetBtn)    //[*** This is an example about functions ***]
+addChild(home, counter)     //[*** This is an example about functions ***]
 
 
+button.addEventListener("click", function () { // *******
+    count++                                    //      **
+    counter.innerText = `Count: ${count}`      //      **
 
+    // --------------- add item "P" every click //     **
 
-button.addEventListener("click", function () {
-    count++
-    counter.innerText = `Count: ${count}`
+    // every time i click the button it will create a new element 
+    // [*** if we create the element outside the CLICK EVENT BLOCK it will be created just once ***]
+    const newP = document.createElement("p")   //      **
 
-    // --------------- add item "P" every click
+    // style the new element                   //      **
+    newP.style.color = "#fff"                  //      **
+    newP.style.fontWeight = "600"              //      **
+    newP.innerHTML = `item ${count}`           //      **
 
-    // create a new element
-    const newP = document.createElement("p")
+    // add the new element to the Document     //      **
+    addChild(home, newP)                       //      **
 
-    // style the new element
-    newP.style.color = "#fff"
-    newP.style.fontWeight = "600"
-    newP.innerHTML = `item ${count}`
-
-    // add the new element to the Document
-    addChild(home, newP)
-
-    // Reset button
+    // Reset button                            //      **
     resetBtn.addEventListener('click', function () {
-        newP.remove()
-        // reset the count
-        count = 0
-        // update the reset on the Document
-        counter.innerText = `Count: ${count}`
-    })
-})
+        newP.remove()                          //      **
+        // reset the count                     //      **
+        count = 0                              //      **
+        // update the reset on the Document    //      **
+        counter.innerText = `Count: ${count}`  //      **
+    })                                         //      **
+})                                             // *******
 
 
-
+// Back to top Button
 const backToTop = document.querySelector('.toTop')
 
 backToTop.addEventListener("click", () => {
@@ -124,3 +123,17 @@ backToTop.addEventListener("click", () => {
     })
 })
 
+// Pressing on burger menu to open the navbar
+
+const icon = document.getElementById("icon")
+const ul = document.getElementsByTagName("ul")[0]
+const items = document.getElementById("items")
+const dorpDownMenu = document.getElementsByTagName("ul")[1]
+
+icon.addEventListener("click", () => {
+    ul.classList.toggle("show-menu")
+})
+
+items.addEventListener("click", () => {
+    dorpDownMenu.classList.toggle("show-menu")
+})
